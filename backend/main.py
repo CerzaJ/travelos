fe0c574180fdd458c91d7edb78be0f333611c4cd
FranciscoOrtega5000 <francisco.ortega6448@alumnos.udg.mx>
2026-03-17 07:46:15 -0600

    Requirementes

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
 
+app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    password: str
